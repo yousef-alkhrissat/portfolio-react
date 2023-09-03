e71b0933678b7f6d97f209581a2c18afbc4d2e13
Yousef Alkhrissat <71035161+yousef-alkhrissat@users.noreply.github.com>
2023-09-04 00:28:34 +0300

    added new cv

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Topbar />
       <Intro />
       <Experience />
-      {/* <Portfolio /> */}
+      <Portfolio />
       <Testimonials />
       <Contact />
       <Footer />
